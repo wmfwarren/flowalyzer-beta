d8d4240f91e942751978e2a87a51bb8592bedc91
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-08-11 00:21:42 -0500

    started base metaphone, on step 6

--- app/controllers/dataEntryCTRL.js
+++ app/controllers/dataEntryCTRL.js
@@ -3,6 +3,7 @@
 app.controller("dataEntryCTRL", function ($scope, $route, dataFactory, metaphoneService) {
 	$scope.flowInput = null;
 	var rawObject = new metaphoneService.RawFlow();
+	var metaphoneObject = new metaphoneService.MetaphonedFlow();
 
 	$scope.input = function (){
 		console.log("Flow to Check", $scope.flowInput);
@@ -11,6 +12,7 @@ app.controller("dataEntryCTRL", function ($scope, $route, dataFactory, metaphone
 		rawObject.lineBreaker();
 		rawObject.wordBreaker();
 		console.log(rawObject, "rawObject");
-
+		metaphoneObject.metaphoner(rawObject.rawInput);
+		console.log("metaphoned obj", metaphoneObject);
 	};
 })
--- app/services/metaphoneService.js
+++ app/services/metaphoneService.js
@@ -19,7 +19,7 @@ app.service("metaphoneService", function(){
 		//methods
 			//method to clean of punctuations
 		this.rawCleaner = function(){
-			this.rawCleaned = this.rawInput.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+			this.rawCleaned = this.rawInput.replace(/[.,\/#!$%\^&\*;'":{}=\-_`~()]/g,"").toLowerCase();
 		};
 			//method to break at line breaks
 		this.lineBreaker = function(){
@@ -40,6 +40,35 @@ app.service("metaphoneService", function(){
 		this.byWordMP = null; // the by word breakdown 2D array
 		//methods
 			//metaphone converter
+		this.metaphoner = function(flow){
+			flow = flow.replace(/\n/, " \n "); //prep for step 2, 3
+			//1. drop double letters to single except c
+			flow = flow.replace(/[^\w\s]|([bdfghjklmnpqrstvwxyzaeiou])(?=\1)/g, "");
+			//2. If the word begins with 'KN', 'GN', 'PN', 'AE', 'WR', drop the first letter
+			flow = flow.replace(/ kn/g, " n");
+			flow = flow.replace(/ gn/g, " n");
+			flow = flow.replace(/ pn/g, " n");
+			flow = flow.replace(/ ae/g, " e");
+			flow = flow.replace(/ wr/g, " r");
+			//3. Drop 'B' if after 'M' at the end of the word.
+			flow = flow.replace(/mb/, "m"); //need to fix
+			//4. 'C' transforms to 'X' if followed by 'IA' or 'H' (unless in latter case, it is part of '-SCH-', in which case it transforms to 'K').
+		  //'C' transforms to 'S' if followed by 'I', 'E', or 'Y'. Otherwise, 'C' transforms to 'K'.
+		  flow = flow.replace(/sch/g, "skh");
+			flow = flow.replace(/cia/g, "xia");
+			flow = flow.replace(/ch/g, "xh");
+			flow = flow.replace(/ci/g, "si");
+			flow = flow.replace(/ce/g, "se");
+			flow = flow.replace(/cy/g, "sy");
+			flow = flow.replace(/c/g, "k");
+			//5. 'D' transforms to 'J' if followed by 'GE', 'GY', or 'GI'. Otherwise, 'D' transforms to 'T'.
+			flow = flow.replace(/dge/g, "j");
+			flow = flow.replace(/dgy/g, "j");
+			flow = flow.replace(/dgi/g, "j");
+			flow = flow.replace(/d/g, "t");
+			//6. 
+			this.cleanedMP = flow;
+		};
 			//stringifier
 			//line seperator
 			//word seperator
